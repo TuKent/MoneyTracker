a4b48158a7c7a2ab6f06129f6908574dc382700f
trannamlong <long.trannam@hotmail.com>
2020-01-06 10:50:43 +0700

    - add password property into User class

--- src/app/moneytracker/model/User.java
+++ src/app/moneytracker/model/User.java
@@ -6,6 +6,8 @@ public class User {
 
     private String username;
 
+    private String password;
+
     public User() {
     }
 
@@ -28,5 +30,13 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
